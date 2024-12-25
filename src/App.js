cadba851aa9a98bd6fabe906e03908f9d87e39a7
guykub <worratod@gmail.com>
2024-12-25 15:02:33 +0700

    add fucn com

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './style.css';
+import { Header,Content,Footer } from './func-components';
+import Calendar from './class-component';
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+    <Header/>
+    <p><center><Calendar/></center></p>
+    <Content/>,<Footer/>
     </>
-  );
+  )
 }
 
 export default App;
