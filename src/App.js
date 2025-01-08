0e350fcc3304cf4659fd59d9277e263669b5c03e
guykub <152575179+guykub@users.noreply.github.com>
2025-01-08 15:30:13 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header,Content,Footer } from './func-components';
 import Calendar from './class-component';
 import Button from './class-components';
+import { Calculator2 } from './calculator';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     {/* <Header/>
     <p><center><Calendar/></center></p>
     <Content/><Footer/> */}
-    <center><br/><Button/></center>
+    {/* <center><br/><Button/></center> */}
+    <center/><br/><Calculator2/>
     </>
   )
 }
