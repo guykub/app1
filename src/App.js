04976969ffacedf02a74f4433ece140463cb43b5
guykub <152575179+guykub@users.noreply.github.com>
2025-01-08 14:16:35 +0700

    Problem Class Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Header,Content,Footer } from './func-components';
 import Calendar from './class-component';
+import Button from './class-components';
 
 function App() {
   return (
     <>
-    <Header/>
+    {/* <Header/>
     <p><center><Calendar/></center></p>
-    <Content/><Footer/>
+    <Content/><Footer/> */}
+    <center><br/><Button/></center>
     </>
   )
 }
