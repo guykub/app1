a07d9a822b8f81515c77e73375544a3ac3ff4f0e
guykub <152575179+guykub@users.noreply.github.com>
2025-01-15 14:14:36 +0700

    add refs-fucn.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Calendar from './class-component';
 import Button from './class-components';
 import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
+import RefsFunc from './refs-func';
 function App() {
   return (
     <>
     {/* <Header/>
     <p><center><Calendar/></center></p>
-    <Content/><Footer/> */}
-    {/* <center><br/><Button/></center> */}
-    {/* <center/><br/><Calculator2/> */}
-    <center><EventData2/></center>
+    <Content/><Footer/>
+    <center><br/><Button/></center>
+    <center/><br/><Calculator2/>
+    <center><EventData2/></center> */}
+    <center><RefsFunc/></center>
     </>
   )
 }
